724398a604d5b3328380e6dd6c361508cde9eacd
Bruno <brunomiguel1997dh@gmail.com>
2026-03-25 14:06:17 +0100

    feat: ajout de la classe Form pour la génération de formulaires

---
+++ app/class/form.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Class Form
+ * 
+ * Permet de générer le code HTML d'un formulaire
+ */
+class Form
+{
+    /**
+     * @var string Contient l'ensemble du code HTML
+     */
+    private string $html;
+
+    /**
+     * Ouvre les balises structurelles du formulaire
+     * @param string $action L'URL de destination du formulaire
+     * @param string $method La méthode POST
+     */
+    public function __construct(string $action, string $method = "post")
+    {
+        $this->html = "<form action=\"$action\" method=\"$method\">\n";
+    }
+
+    /**
+     * Crée une balise label et un input du type text à l'intérieur d'un paragraphe
+     *
+     * @param string $name Le nom de l'attribut
+     * @param string $label La description affichée pour l'utilisateur
+     * @param string $type Le type d'input (text, password, email, etc.).
+     * @return void
+     */
+    public function setInput(string $name, string $label, string $type): void
+    {
+        $this->html .= "<p>";
+        $this->html .= "<label for =\"$name\">$label</label>\n";
+        $this->html .= "<input type = \"$type\" name =\"$name\" id=\"$name\">";
+        $this->html .= "</p>";
+    }
+
+    /**
+     * Crée un paragraphe avec un input du type submit intérieur
+     * @param string $value la value du input
+     * @return void
+     */
+    public function setSubmit(string $value): void
+    {
+        $this->html .= "<p>\n";
+        $this->html .= "<input type=\"submit\" value=\"$value\">\n";
+        $this->html .= "</p>\n";
+    }
+
+    /**
+     * Ferme les balises structurelles du formulaire
+     * @return string formulaire crée
+     */
+    public function getForm(): string
+    {
+        $form = $this->html;
+        $form .= "</form>";
+
+        return $form;
+    }
+}
