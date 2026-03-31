c38128f3081da44f3441b402546aeab53ee4a3e9
Bruno <brunomiguel1997dh@gmail.com>
2026-03-31 16:03:21 +0200

    chore: nettoyage des commentaires dans register.php

--- app/view/register.php
+++ app/view/register.php
@@ -1,11 +1,11 @@
-<!--  ==================== Head ==================== -->
+<?php // ==================== Head ==================== ?>
 <?php require_once RACINE . '/app/view/partials/head.php'; ?>
 
 <body>
-    <!--  ==================== Header ==================== -->
+    <?php // ==================== Header ==================== ?>
     <?php require RACINE . '/app/view/partials/header.php'; ?>
     <h1>S'inscrire</h1>
-    <!-- ==================== Formulaire ==================== -->
+    <?php // ==================== Formulaire ==================== ?>
     <?= $formRegister; ?>
 
 </body>
