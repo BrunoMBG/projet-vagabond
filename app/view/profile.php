e890a9c77cf9ca147718e9236c72e1d96eb8aa3e
Bruno <brunomiguel1997dh@gmail.com>
2026-04-01 14:48:14 +0200

    chore: nettoyage des commentaires dans profile.php

--- app/view/profile.php
+++ app/view/profile.php
@@ -1,10 +1,8 @@
-<?php // ==================== Head ==================== 
-?>
+<?php // ==================== Head ==================== ?>
 <?php require_once RACINE . '/app/view/partials/head.php'; ?>
 
 <body>
-    <?php // ==================== Header ==================== 
-    ?>
+    <?php // ==================== Header ==================== ?>
     <?php require RACINE . '/app/view/partials/header.php'; ?>
     <!--====================  Main ==================== -->
     <main>
@@ -16,23 +14,21 @@
             <p><strong>Prénom :</strong> <?= htmlspecialchars($user['prenom']) ?></p>
             <p><strong>Email :</strong> <?= htmlspecialchars($user['email']) ?></p>
             <?php
-            /**
-             * Gestion du libellé du rôle
-             * On transforme l'ID numérique de la base de données en texte.
-             */
-            $nomsRoles = [
-                1 => "Administrateur",
-                2 => "Rédacteur",
-                3 => "Membre"
-            ];
+                /**
+                 * Gestion du libellé du rôle
+                 * Récupération l'ID numérique de la base de données en texte.
+                 */
+                $nomsRoles = [
+                    1 => "Administrateur",
+                    2 => "Rédacteur",
+                    3 => "Membre"
+                ];
 
-            // On récupère le nom associé à l'ID stocké en session, 
-            // sinon on affiche "Utilisateur" par défaut
-            $roleLibelle = $nomsRoles[$user['role']] ?? "Utilisateur";
+                // Récupération du nom associé à l'ID stocké en session, 
+                // sinon on affiche "Utilisateur" par défaut
+                $roleLibelle = $nomsRoles[$user['role']] ?? "Utilisateur";
             ?>
             <p><strong>Rôle : <?= htmlspecialchars($roleLibelle) ?></strong> </p>
-
-
         </div>
     </main>
 </body>
