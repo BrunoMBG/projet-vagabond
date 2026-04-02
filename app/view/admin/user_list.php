3f3365a85470b69b4f9fce79d10001e3855d9f0a
Bruno <brunomiguel1997dh@gmail.com>
2026-04-02 12:21:03 +0200

    feat(view): implémentation complète de l'interface de gestion des utilisateurs

--- app/view/admin/user_list.php
+++ app/view/admin/user_list.php
@@ -1,11 +1,62 @@
 <?php // ==================== Head ==================== ?>
+
 <?php require_once RACINE . '/app/view/partials/head.php'; ?>
 
 <body>
     <?php // ==================== Header ==================== ?>
     <?php require RACINE . '/app/view/partials/header.php'; ?>
-    <h1>Gestion des utilisateurs</h1>
 
+    <main">
+        <div">
+            <!-- Vérifie s'il utilisateur est admin -->
+            <?php if ($_SESSION['user_role'] == 1): ?>
+                <section>
+                    <h2>Gestion des Utilisateurs</h2>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Nom</th>
+                                <th>Rôle</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php // Boucle sur chaque utilisateur récupéré via le contrôleur ?>
+                            <?php foreach ($users as $user): ?>
+                                <tr>
+                                    <td><?= htmlspecialchars($user['prenom'] . " " . $user['nom']) ?></td>
+                                    <td>
+                                        <span">
+                                            <?php // Affiche le labelle de chaque rôle ?>
+                                            <?= ($user['id_role'] == 1) ? 'Admin' : (($user['id_role'] == 2) ? 'Rédacteur' : 'Membre') ?>
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <form action="index.php?action=user_update_role" method="POST" class="form-role-quick">
+                                            <input type="hidden" name="id_utilisateur" value="<?= $user['id_utilisateur'] ?>">
+
+                                            <?php // Pré-sélection du rôle actuel ?>
+                                            <select name="new_role" onchange="this.form.submit()">
+                                                <option value="1" <?= ($user['id_role'] === 1) ? 'selected' : '' ?>>Admin</option>
+                                                <option value="2" <?= ($user['id_role'] === 2) ? 'selected' : '' ?>>Rédacteur</option>
+                                                <option value="3" <?= ($user['id_role'] === 3) ? 'selected' : '' ?>>Membre</option>
+                                            </select>
+
+                                            <?php // Boutton de validation ?>
+                                            <button type="submit">OK</button>
+
+                                        </form>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </section>
+            <?php endif; ?>
+
+            </div>
+            </main>
 </body>
 
 </html>
