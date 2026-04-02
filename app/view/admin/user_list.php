4584ee01334944bcd19e1e0d0547ade1b8620494
Bruno <brunomiguel1997dh@gmail.com>
2026-04-02 14:59:29 +0200

    feat(view): finalisation de la table de gestion des utilisateurs

--- app/view/admin/user_list.php
+++ app/view/admin/user_list.php
@@ -16,28 +16,32 @@
                     <h2>Gestion des Utilisateurs</h2>
 
                     <table>
+                        <!-- Les titres du tableau -->
                         <thead>
                             <tr>
+                                <th>Prénom</th>
                                 <th>Nom</th>
                                 <th>Rôle</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
+                        <!-- Les cellules -->
                         <tbody>
                             <?php // Boucle sur chaque utilisateur récupéré via le contrôleur 
                             ?>
                             <?php foreach ($users as $user): ?>
                                 <tr>
-                                    <td><?= htmlspecialchars($user['prenom'] . " " . $user['nom']) ?></td>
+                                    <td><?= htmlspecialchars($user['prenom']) ?></td>
+                                    <td><?= htmlspecialchars($user['nom']) ?></td>
                                     <td>
                                         <span">
                                             <?php // Affiche le labelle de chaque rôle 
                                             ?>
-                                            <?= ($user['id_role'] == 1) ? 'Admin' : (($user['id_role'] == 2) ? 'Rédacteur' : 'Membre') ?>
+                                            <?= htmlspecialchars($user['libelle']) ?>
                                             </span>
                                     </td>
                                     <td>
-                                        <form action="index.php?action=user_update_role" method="POST" class="form-role-quick">
+                                        <form action="index.php?action=user_update_role" method="POST">
                                             <input type="hidden" name="id_utilisateur" value="<?= $user['id_utilisateur'] ?>">
 
                                             <?php // Pré-sélection du rôle actuel 
