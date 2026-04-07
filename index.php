19a6f5ae6b9c326607a393a6e4fe9ee4671a37e0
Bruno <brunomiguel1997dh@gmail.com>
2026-04-07 11:41:30 +0200

    fix: correction du flux d'affichage pour le service d'images

--- index.php
+++ index.php
@@ -1,23 +1,38 @@
-    <?php // ==================== Config global ==================== ?>  
-    <?php session_start(); ?>
-    <?php require __DIR__ . '/app/config/config.php'; ?>
+  <?php session_start(); ?>
+  <?php // ==================== Config global ==================== ?>
+  <?php require __DIR__ . '/app/config/config.php'; ?>
 
-    <?php // ==================== Head ==================== ?>  
-    <?php require RACINE . '/app/view/partials/head.php'; ?>
+  <?php // ==================== Connexion à la base de données ==================== ?>
+  <?php require RACINE . '/app/model/db_connection.php'; ?>
 
-    <?php // ==================== Connexion à la base de données ==================== ?>
-    <?php require RACINE . '/app/model/db_connection.php'; ?>
+  <?php // ==================== Routage ==================== ?>
+  <?php require RACINE . '/app/controller/router.php'; ?>
 
-    <body>
-      <?php // ==================== Routage ==================== ?>
-      <?php require RACINE . '/app/controller/router.php'; ?>
 
-      <?php
-      // On récupère l'action de l'URL, sinon on utilise "default"
-      $action = $_GET["action"] ?? "default";
-      // Appel de la fonction de routage
+  <?php
+    // On récupère l'action de l'URL, sinon on utilise "default"
+    $action = $_GET["action"] ?? "default";
+
+    if ($action === 'viewImage') {
       handleRequest($action);
-      ?>
-    </body>
+      exit; 
+    }
+  ?>
 
-    </html>
+  <?php // ==================== Head ==================== ?>
+  <?php require RACINE . '/app/view/partials/head.php'; ?>
+
+  <body>
+    <?php // ==================== Header ==================== ?>
+    <?php require RACINE . '/app/view/partials/header.php'; ?>
+
+    <main>
+      <?php
+        // Appel de la fonction de routage
+        handleRequest($action);
+      ?>
+    </main>
+
+  </body>
+
+  </html>
