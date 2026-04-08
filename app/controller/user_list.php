67deb3b4bbb45aa656e71abef9f6d7928e14ab33
Bruno <brunomiguel1997dh@gmail.com>
2026-04-08 11:24:17 +0200

    docs: mise à jour du commentaire de user_list

--- app/controller/user_list.php
+++ app/controller/user_list.php
@@ -1,6 +1,16 @@
 <?php
-require RACINE . "/app/model/user.php";
+    /**
+     * Contrôleur de la liste des utilisateurs.
+     * * user_list récupère l'ensemble des utilisateurs depuis le modèle 
+     * et les transmet à la vue correspondante pour gérer l'affichage.
+     * * @return void
+     */
+?>
+
+<?php
+
+    require RACINE . "/app/model/user.php";
 
 
 
-require RACINE . "/app/view/user_list.php";
+    require RACINE . "/app/view/user_list.php";
