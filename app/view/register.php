8dc4867658635dc40b3787d7df0165d9e8a89455
Bruno <brunomiguel1997dh@gmail.com>
2026-04-08 13:58:00 +0200

    style: restructuration sémantique et documentation de la vue inscription

--- app/view/register.php
+++ app/view/register.php
@@ -1,13 +1,22 @@
 <?php
+
 /**
- * Vue Inscription 
- * 
- * Affiche l'interface de création de compte pour les nouveaux visiteurs :
- * $formRegister (string) : Bloc HTML complet généré par la classe 'Form'.
+ * ARCHITECTURE : Vue Inscription (Register)
+ * * Affiche l'interface de création de compte pour les nouveaux visiteurs :
+ * - $formRegister (string) : Bloc HTML complet généré par la classe 'Form'.
  */
+
 ?>
-<h1>S'inscrire</h1>
-<?php // ==================== Formulaire ==================== ?>
-<section>
-    <?= $formRegister; ?>
+
+<section class="">
+    <div class="">
+        
+        <header class="">
+            <h1>Créer un compte</h1>
+        </header>
+
+        <div class="">
+            <?= $formRegister; ?>
+        </div>
+    </div>
 </section>
