92cafb72ac53c744577290f8b4e448fff37e1334
Bruno <brunomiguel1997dh@gmail.com>
2026-04-10 10:23:07 +0200

    feat(model): ajouter la récupération des destinations visitées

--- app/model/destinations.php
+++ app/model/destinations.php
@@ -8,6 +8,7 @@
  * modification d'articles en listant les lieux disponibles.
  * 
  * getAllDestinations, récupère toutes les destinations classées par ordre alphabétique.
+ * getVisitedDestinations, Récupère la liste des destinations ayant au moins un récit associé.
  */
 
 /**
@@ -20,6 +21,24 @@
 function getAllDestinations(PDO $db): array
 {
     $sql = "SELECT id_destination, nom_destination FROM destinations ORDER BY nom_destination ASC";
+    $query = $db->query($sql);
+    return $query->fetchAll(PDO::FETCH_ASSOC);
+}
+
+
+/**
+ * Récupère la liste des destinations ayant au moins un récit associé.
+ * et ne retourner que les lieux réellement visités.
+ * @param PDO $db Connexion à la base de données.
+ * @return array Liste associative des destinations triées par ordre alphabétique.
+ */
+function getVisitedDestinations(PDO $db): array
+{
+    $sql = "SELECT DISTINCT d.id_destination, d.nom_destination 
+            FROM destinations d
+            INNER JOIN recits r ON d.id_destination = r.id_destination
+            ORDER BY d.nom_destination ASC";
+
     $query = $db->query($sql);
     return $query->fetchAll(PDO::FETCH_ASSOC);
 }
