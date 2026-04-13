a53dcd55e8beb7f5f5e31c1ba14c06edf225a85c
Bruno <brunomiguel1997dh@gmail.com>
2026-04-13 09:57:37 +0200

    feat: ajout du style  pour la section derniers voyages et intégration de la police Roboto/Poppins

--- app/view/home.php
+++ app/view/home.php
@@ -16,39 +16,39 @@
 </section>
 
 <?php // Section des derniers articles ?>
-<section class="">
-    <div class="">
+<section class="last-articles-container">
+    <div class="last-articles-">
 
         <?php // Titre de la section ?>
-        <header class="">
+        <header class="last-articles-title">
             <h2>Mes derniers voyages</h2>
         </header>
         
          <?php // Liste des articles ?>
-        <div class="">
+        <div class="articles-container">
             <?php if (!empty($articles)): ?>
 
                 <?php // Boucle sur les articles ?>
                 <?php foreach ($articles as $article): ?>
-                    <article class="">
+                    <article class="last-article">
 
                          <?php // Vérifie si une image est associée à l'article ?>
                         <?php if (!empty($article['image'])): ?>
-                            <div class="">
+                            <div class="articles-images">
                                 <img src="index.php?action=viewImage&name=<?= urlencode($article['image']) ?>" 
                                      alt="<?= htmlspecialchars($article['titre']) ?>">
                             </div>
                         <?php endif; ?>
 
                         <?php // Titre de l'article ?>
-                        <div class="">
+                        <div class="last-article-content">
                             <h3><?= htmlspecialchars($article['titre']) ?></h3>
 
                             <?php // Extrait du contenu (100 caractères max) ?>
                             <p><?= nl2br(htmlspecialchars(substr($article['contenu'], 0, 100))) ?>...</p>
                             
                             <?php // Lien vers la page détaillée de l'article ?>
-                            <div class="">
+                            <div class="last-article-btn">
                                 <a href="index.php?action=articleView&id=<?= $article['id_recit'] ?>" class="btn-read">
                                     Lire la suite
                                 </a>
