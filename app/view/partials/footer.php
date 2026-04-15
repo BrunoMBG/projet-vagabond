d11b2993bd7e21ff4f35af79eff5a25e2d7a7ed6
Bruno <brunomiguel1997dh@gmail.com>
2026-04-15 16:11:15 +0200

    style: restructuration du footer avec SCSS  et nouveaux liens de navigation

--- app/view/partials/footer.php
+++ app/view/partials/footer.php
@@ -5,11 +5,18 @@
  * Affiche le copyright et le lien vers les mentions légales.
  */
 ?>
+
 <footer class="footer-container" role="contentinfo">
     <div class="footer-content">
-        <p>
-            &copy; 2026 
-            <a href="index.php?action=legal_Notice" class="footer-link" aria-label="Consulter les mentions légales">Mentions légales</a>
-        </p>
+        <?php // copyright ?>
+        <div class="footer-info">
+            <p>&copy; 2026 <strong>Vagabond</strong> - Tous droits réservés</p>
+        </div>
+        <?php // Menu ?>
+        <nav class="footer-link" aria-label="Liens secondaires">
+            <a href="index.php?action=legal_Notice" class="footer-link-nav">Mentions légales</a>
+            <span class="border">|</span>
+            <a href="index.php?action=contact" class="footer-link-nav">Contact</a>
+        </nav>
     </div>
 </footer>
