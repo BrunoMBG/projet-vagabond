87968e13b8583f0812aa2ee9b2fda13309bfcb13
Bruno <brunomiguel1997dh@gmail.com>
2026-04-15 11:08:12 +0200

    style(404): finalisation de la vue et du style

--- app/view/page404.php
+++ app/view/page404.php
@@ -7,6 +7,20 @@
  */
 
 ?>
-<h1>
-    404 : Page introuvable
-</h1>
+
+<div class="error-container">
+    <section class="error" aria-labelledby="error-title">
+        <h1 id="error-title">Oups ! Ce chemin n'existe pas.</h1>
+        
+        <div class="error-content">
+            <p class="error-code">Erreur 404</p>
+            <p>Il semble que vous vous soyez égaré en chemin. Pas de panique, même les meilleurs vagabonds se perdent parfois !</p>
+        </div>
+
+        <div class="error-actions">
+            <a href="index.php?action=default" class="btn-read" aria-label="Retourner à la page d'accueil">
+               Accueil
+            </a>
+        </div>
+    </section>
+</div>
